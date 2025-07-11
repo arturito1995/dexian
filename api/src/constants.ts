35c1d76a05b803bf902fdc1736714f7305eb75fc
Arthur Bortolini <arthur.bortolini@ef.com>
2025-07-10 21:09:00 -0300

    adds layer powertool dependencies

--- api/src/constants.ts
+++ api/src/constants.ts
@@ -1,10 +1,4 @@
-export const EXTERNAL_MODULES = [
-  "joi",
-  "@aws-lambda-powertools/metrics",
-  "@aws-lambda-powertools/logger",
-  "@aws-lambda-powertools/tracer",
-  "@aws-sdk/*",
-];
+export const EXTERNAL_MODULES = ["joi", "@aws-lambda-powertools/*", "@aws-sdk/*"];
 export const DEFAULT_AWS_REGION = "sa-east-1";
 
 export const DEFAULT_LAMBDA_HEADERS = {
